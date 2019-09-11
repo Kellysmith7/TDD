5422f2ea9473d01cca6b9142ccc0c0c39ff8d6cc
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-10 21:26:44 -0500

    TDD Project

---
+++ main/camper_age_input.py
@@ -0,0 +1,19 @@
+"""
+Program: camper_age_input.py
+Author: Kelly Smith
+Last date updated: 09/03/2019
+
+Program to convert a child's age from years to months
+"""
+
+
+def convert_to_months(x):
+    months_years = 12
+    years_in_months = x*months_years
+    return years_in_months
+
+
+if __name__ == '__main__':
+    age = input('Please enter child age in years')
+    y = convert_to_months(age)
+    print(y)
---
+++ test/test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from main import camper_age_input
+
+
+class FunctionTestCase(unittest.TestCase):
+    def test_convert_age_months(self):
+        self.assertEqual(camper_age_input.convert_to_months(3),36)
+
+
+if __name__ == '__main__':
+    unittest.main()
